94ff9d2b3b10763e359d4233ae18503b2f598ea6
erkamesen <bmth_789@hotmail.com>
2023-04-29 12:52:14 +0300

    Update

--- utils.py
+++ utils.py
@@ -1,10 +1,18 @@
-import requests
+import qrcode
+import time
+import os
+
+def generate_qrcode(url):
+    qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,)
+    qr.add_data(url)
+    qr.make(fit=True)
+    code = qrcode.make(url)
+    img = qr.make_image(back_color=(255, 195, 235), fill_color=(55, 95, 35))
+    
 
 
-
-def qr_code(id):
-    r = requests.get(f"https://api.qrserver.com/v1/create-qr-code/?size=150x150&data={id}")
-    return r
-
-
-
+generate_qrcode("http://www.batkarfest.com")
